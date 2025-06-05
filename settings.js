77173c1811289859c9c9fea38a0251131ca2a07f
Sangoku400 <botwatchappserveur@gmail.com>
2025-06-05 06:13:01 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "malvin~giVTTajb#JYU2eq-Qv1GOuOByX3auZvmty1GS4e0TBG3MzEsb-bw",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
